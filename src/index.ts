012cbe6b224cc6cc164a86f2a9a8a89021fe55ca
Matthew Lean <matt@mattlean.com>
2021-04-17 23:35:08 -0700

    Add logAll option, add minor updates to config - Add logAll option - Add tests with Redux Thunk - Add .npmignore - Update package.json description

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,24 @@ export class ReduxActionsLog {
   /** @member actions Array that stores Redux actions. */
   private actions: Array<AnyAction> = []
 
+  /** @member logAll Option that controls whether or not log all functionality is enabled or not */
+  private logAll = false
+
+  /** Creates an instance of ReduxActionsLog with a given log all setting. */
+  constructor(logAll = false) {
+    if (logAll) this.logAll = logAll
+  }
+
   /** "Private" method that pushes Redux action onto actions member. */
   _push(action: AnyAction): void {
-    this.actions.push(action)
+    if (
+      this.logAll ||
+      (typeof action === 'object' &&
+        action !== null &&
+        action.type !== undefined)
+    ) {
+      this.actions.push(action)
+    }
   }
 
   /** Get logged actions that were dispatched to a Redux store. */
@@ -19,11 +34,23 @@ export class ReduxActionsLog {
   clearActions(): void {
     this.actions.splice(0, this.actions.length)
   }
+
+  /** Enable or disable log all option. */
+  setLogAll(logAll: boolean): void {
+    this.logAll = logAll
+  }
+
+  /** Get current log all option. */
+  getLogAll(): boolean {
+    return this.logAll
+  }
 }
 
-/** Setup Redux actions log. */
-const setupReduxActionsLog = (): [Middleware, ReduxActionsLog] => {
-  const log = new ReduxActionsLog()
+/** Setup Redux actions log with a given log all setting. */
+const setupReduxActionsLog = (
+  logAll = false
+): [Middleware, ReduxActionsLog] => {
+  const log = new ReduxActionsLog(logAll)
 
   /** redux-actions-log Redux middleware */
   const reduxActionsLogMiddleware: Middleware = () => (next) => (action) => {
